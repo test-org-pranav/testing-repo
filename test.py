d56973f056c60f518e14bde967eae46e6fcc453d
pranavmaiya <pranav.maiya@couchbase.com>
2026-05-26 16:41:00 +0530

    test commit 1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 num=10
 print(num,"this is test1.py")
-import test2
+import iterate_files
 print(num,"this is test1.py")
 print("Hello")
 print("World")
--- test2.py
+++
@@ -1,5 +0,0 @@
-import test
-# print(test.num)
-test.num=20
-# print(__name__)
-print(test.num,"this is test2.py")
