20587d7a6f4423e011b10a046328c1331fc882b0
pranavmaiya1894 <113848485+pranavmaiya1894@users.noreply.github.com>
2026-06-02 14:10:31 +0530

    Update iterate_files.py

--- iterate_files.py
+++ iterate_files.py
@@ -24,3 +24,8 @@ for f in os.listdir(dir):
     if ext == '.jpg':
         new_name = basename.split('_')[0].lower() + ext
         os.rename(os.path.join(dir, f), os.path.join(dir, new_name))
+
+
+
+########
+this is what i am adding now , please look 
