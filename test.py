475047e6a751909425cb745c8d6ebeec828f39f3
pranavmaiya <pranav.maiya@couchbase.com>
2026-06-02 22:51:49 +0530

    PR STAGE 3 Testing 1  (#8)
    * Update test.py
    
    * Update test.py
    
    * Update test.py stage 3
    
    * Update test.py
    
    * Update test.py
    
    * Update test.py
    
    ---------
    
    Co-authored-by: pranavmaiya1894 <113848485+pranavmaiya1894@users.noreply.github.com>
    Co-authored-by: PES2UG22CS400 <pes2ug22cs400@pesu.pes.edu>

--- test.py
+++ test.py
@@ -9,4 +9,10 @@ print("hello , adding this line now , again ")
 #####
 print("this is what i am typing now")
 printe("added this line now ")
-
+#########
+this is the edit we are making as a part of stage 3 testing 
+and i just added a new line here 
+i also added a new line here 
+this line was added now 
+pranavmaiya1894 adding a new commit here 
+adding one simple commit for no reason 
